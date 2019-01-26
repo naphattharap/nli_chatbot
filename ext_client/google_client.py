71ba7195f80ce162e52fb62331dd62089cc60c78
Naphatthara <>
2019-01-26 21:03:04 +0100

    Finished google client

--- ext_client/google_client.py
+++ ext_client/google_client.py
@@ -1,72 +1,113 @@
 import json
 import urllib.request
+import logging
+"""
+Find books' information by calling API.
+https://developers.google.com/books/docs/v1/using
+"""
+logging.getLogger().setLevel(logging.DEBUG)
 
 
-# class GoogleApiClientService:
-#     def __init__(self):
-#         API_KEY = "AIzaSyBiepAN_JJFNYeDHN7yGqyfWWNr8SHQpL8"# copied from project credentials page
-#         self.GPLUS = discovery.build('plus', 'v1', developerKey=API_KEY)
-# 
-#     def search_by_genre(self, genre):
-# 
-#         items = self.GPLUS.activities().search(query='subject:'+genre).execute().get('items', [])
-#         for data in items:
-#             post = ' '.join(data['title'].strip().split())
-#             if post:
-#                 print(TMPL % (data['actor']['displayName'],
-#                               data['published'], post))
 class GoogleBookApiService:
 
     def __init__(self):
         # https://www.googleapis.com/books/v1/volumes?q=harry%20potter&maxResults=1&startIndex=100
         self.max_result = 10
 
-    def recommend_book_by_genre_intent(self, genre):  
-        
-        base_api_link = "https://www.googleapis.com/books/v1/volumes?q=subject:" + genre + "&maxResult=5"
-    
+    def get_json_respond(self, base_api_link):
+        """
+        Execute request URL and return as JSON object
+        """
+        # encode space to %20
+        base_api_link = base_api_link.replace(" ", "%20")
+        # logging.debug("encoded URL: %s", base_api_link)
         with urllib.request.urlopen(base_api_link) as f:
             text = f.read()
     
         decoded_text = text.decode("utf-8")
-        print(decoded_text)
-        obj = json.loads(decoded_text)  # deserializes decoded_text to a Python object
-        items = obj["items"]
-        titles = []
+        # logging.debug("respond json: %s", decoded_text)
+        json_obj = json.loads(decoded_text)  # deserializes decoded_text to a Python object
+        items = json_obj["items"]
+       
+        book_list = []
         for item in items:
-            titles.append(item["volumeInfo"]["title"])
+            volumn_info = item["volumeInfo"]
+            sale_info = item["saleInfo"]
+            book = {}
+            book["title"] = volumn_info["title"]
+            book["authors"] = volumn_info["authors"]
+            if 'description' in volumn_info:
+                book["description"] = volumn_info["description"]
+            else:
+                book["description"] = ""
+            
+            if 'categories' in volumn_info:
+                book["categories"] = volumn_info["categories"]
+            else:
+                book["categories"] = ""
+            
+            if 'retailPrice' in sale_info:
+                retail_price = sale_info["retailPrice"]
+                book["retail_price"] = retail_price["amount"]
+                book["currency_code"] = retail_price["currencyCode"]
+            else:
+                book["retail_price"] = ""
+                book["currency_code"] = ""
+            
+            logging.debug("book info: %s", book)
+            book_list.append(book)
         
-        str_title = ""
-        if len(titles) > 0:
-            str_title = ",".join(titles)
-        return str_title
+        return book_list
     
-    def recommend_book_by_author_intent(self, author):
+    def google_book_by_title_intent(self, title, max_result):  
         """
+        Get title, authors, description, category (genre) and retail price
         """
-        base_api_link = "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + author + "&maxResult=1"
-    
-        with urllib.request.urlopen(base_api_link) as f:
-            text = f.read()
-    
-        decoded_text = text.decode("utf-8")
-
-        # deserializes decoded_text to a Python object
-        obj = json.loads(decoded_text)
-        items = obj["items"]
+        # ex. https://www.googleapis.com/books/v1/volumes?q=subect:Harry%20Potter&maxResults=1
+        base_api_link = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + title + "&maxResults=" + str(max_result)
+        book_list = self.get_json_respond(base_api_link)
+        
         results = []
-        for item in items:
-            volume_info = item["volumeInfo"]
-            results.append(volume_info["title"])
-            results.append(volume_info["authors"])
-            # Plot
-            results.append(volume_info["description"])
+        for book in book_list:
+            title = book["title"]
+            if title != "":
+                results.append(title)
         
-#         str_result = ""
-#         if len(results) > 0:
-#             str_result = ",".join(results)
+        logging.debug("recommend_book_by_title_intent: %s", results)
         return results
     
+    def google_book_by_genre_intent(self, genre, max_result):  
+        """
+        Get book information by genre
+        """
+        base_api_link = "https://www.googleapis.com/books/v1/volumes?q=subject:" + genre + "&maxResults=" + str(max_result)
+        book_list = self.get_json_respond(base_api_link)
+        
+        results = []
+        for book in book_list:
+            title = book["title"]
+            if title != "":
+                results.append(title)
+                
+        logging.debug("recommend_book_by_genre_intent: %s", results)
+        return results
+    
+    def google_book_by_author_intent(self, author, max_result):
+        """
+        Get book information by author
+        """
+        base_api_link = "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + author + "&maxResults=" + str(max_result)
+        book_list = self.get_json_respond(base_api_link)
+        
+        results = []
+        for book in book_list:
+            title = book["title"]
+            if title != "":
+                results.append(title)
+        logging.debug("recommend_book_by_author_intent: %s", results)
+        return results
+  
 # googleBook = GoogleBookApiService() 
-# # googleBook.search_by_genre("Art")
-# googleBook.recommend_book_by_author_intent("JK")
+# googleBook.google_book_by_title_intent("Harry Potter", 5)
+# googleBook.google_book_by_genre_intent("Fiction", 5)
+# googleBook.google_book_by_author_intent("Rowling", 5)
